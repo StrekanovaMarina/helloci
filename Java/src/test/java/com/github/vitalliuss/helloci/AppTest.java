7b99f8c26058ebfc5a64f51e3c61f526266d3f6a
StrekanovaMarina <mstrekanova@gmail.com>
2018-02-22 08:37:59 +0100

    Update AppTest.java

--- Java/src/test/java/com/github/vitalliuss/helloci/AppTest.java
+++ Java/src/test/java/com/github/vitalliuss/helloci/AppTest.java
@@ -20,20 +20,4 @@ public class AppTest {
 	public void testShouldBeFailed() {
 		assertTrue(false);
 	}
-
-	@Ignore("Not implemented yet")
-	@Test
-	public void testShouldBeSKipped() {
-		assertTrue(true);
-	}
-	
-	@Test
-	public void testAnotherMethod() {
-		App.doNothing();
-	}
-	
-	@Test
-	public void testAnotherClass() {
-		EmptyClass.emptyMethod();
-	}
 }
